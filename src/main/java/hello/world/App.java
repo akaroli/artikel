668727357e436fc5733319166399af2890e71f9e
vocislav <anakaroli.001@gmail.com>
2017-11-13 00:01:32 +0100

    added buttons

--- src/main/java/hello/world/App.java
+++ src/main/java/hello/world/App.java
@@ -13,11 +13,15 @@ public class App
 {
 	private static HashMap<String,String> words = new HashMap<String, String>();
 	
+	// http://localhost:4567/hello.html
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         
         words.put("Haus", "das");
+        words.put("Zimmer", "das");
+        words.put("Hund", "der");
+        words.put("Blume", "die");
         
         staticFiles.location("/public");
         get("/hello", (req, res) -> {
@@ -26,6 +30,7 @@ public class App
         });
         
         get("/words", (req, res) -> {
+    		res.type("application/json");
         	Gson gson = new Gson();
         	String json = gson.toJson(words);
         	return json;
